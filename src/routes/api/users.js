226cf326fdd576239efd99ba4d8f2e0b9bbe7d4b
Andrew Brainerd <andrew.brainerd@llamasoft.com>
2019-09-15 18:18:56 -0400

    Remove current route

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -48,17 +48,4 @@ usersRouter.post('/login', auth.optional, (req, res, next) => {
   })(req, res, next);
 });
 
-usersRouter.get('/current', auth.required, (req, res, next) => {
-  const { payload: { id } } = req;
-
-  return Users.findById(id)
-    .then((user) => {
-      if (!user) {
-        return res.sendStatus(400);
-      }
-
-      return res.json({ user: user.toAuthJSON() });
-    });
-});
-
 module.exports = usersRouter;
