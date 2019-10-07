aa606473fff3c094dcd67393605c20d13e0c035e
Andrew Brainerd <andrew.brainerd@llamasoft.com>
2019-10-07 17:23:29 -0400

    Add join and leave pod

--- src/routes/api/pods.js
+++ src/routes/api/pods.js
@@ -1,4 +1,5 @@
 const pods = require('express').Router();
+const ObjectId  = require('mongodb').ObjectId ;
 const data = require('../../utils/data');
 const log = require('../../utils/log');
 const { PODS_COLLECTION } = require('../../constants/collections');
@@ -41,7 +42,6 @@ pods.post('/', async (req, res) => {
 });
 
 pods.get('/', async (req, res) => {
-  log.info(`Get Pods`);
   const { pageNum, pageSize } = req.query;
   const page = parseInt(pageNum) || 1;
   const size = parseInt(pageSize) || 0;
@@ -66,6 +66,78 @@ pods.get('/', async (req, res) => {
       totalPages
     });
   });
+});
+
+pods.get('/:podId', async (req, res) => {
+  const { params: { podId } } = req;
+
+  if (!podId) {
+    return res.status(400).send({
+      message: `Missing query param: [podId]`
+    });
+  }
+
+  const collection = data.db.collection(PODS_COLLECTION);
+  const pod = collection.find({ _id: ObjectId(podId) });
+
+  log.cool(`Get pod ${podId}`);
+
+  return pod.toArray((err, items) => {
+    if (err) throw err;
+    res.send({ ...items[0] });
+  });
+});
+
+pods.patch('/:podId/members', async (req, res) => {
+  const { params: { podId }, body: { user } } = req;
+
+  if (!podId) {
+    return res.status(400).send({
+      message: `Missing query param: [podId]`
+    });
+  }
+
+  if (!user) {
+    return res.status(400).send({
+      message: `Missing body param: [user]`
+    });
+  }
+
+  const collection = data.db.collection(PODS_COLLECTION);
+  const updatedPod = collection.updateOne(
+    { _id: ObjectId(podId) },
+    { $addToSet: { members: user } }
+  );
+
+  const message = `Added member ${user.name} to pod ${podId}`;
+  log.success(message);
+  res.status(200).send({ message })
+});
+
+pods.delete('/:podId/members', async (req, res) => {
+  const { params: { podId }, body: { user } } = req;
+
+  if (!podId) {
+    return res.status(400).send({
+      message: `Missing query param: [podId]`
+    });
+  }
+
+  if (!user) {
+    return res.status(400).send({
+      message: `Missing body param: [user]`
+    });
+  }
+
+  const collection = data.db.collection(PODS_COLLECTION);
+  const updatedPod = collection.updateOne(
+    { _id: ObjectId(podId) },
+    { $pull: { members: user } }
+  );
+
+  const message = `Removed member ${user.name} from pod ${podId}`;
+  log.success(message);
+  res.status(200).send({ message })
 });
 
 module.exports = pods;
