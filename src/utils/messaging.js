4317d47aae5f94310bf01b7aa2319218e41c0725
Andrew Brainerd <andrew.brainerd@llamasoft.com>
2019-10-15 23:59:24 -0400

    Send text on pod creation

--- src/utils/messaging.js
+++ src/utils/messaging.js
@@ -5,11 +5,15 @@ const client = new RestClient(
   process.env.SIGNALWIRE_API_KEY
 );
 
-client.messages
-  .create({
-    from: process.env.SIGNALWIRE_PHONE_NUMBER,
-    body: 'Hello World!',
-    to: '+19897210902'
-  })
-  .then(message => console.log(message))
-  .done();
+exports.sendSms = body => {
+  client.messages
+    .create({
+      from: process.env.SIGNALWIRE_FROM_PHONE_NUMBER,
+      to: process.env.SIGNALWIRE_TO_PHONE_NUMBER,
+      body: body || 'Empty Message'
+    })
+    .then(message => {
+      console.log(`Sending message to ${message.to}: [${message.body}]`);
+    })
+    .done();
+};
