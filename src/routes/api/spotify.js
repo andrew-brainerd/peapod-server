d9ff4772c40bcace152ce8a557eef22ec17a3c92
Andrew Brainerd <andrew.brainerd@llamasoft.com>
2020-01-04 14:25:02 -0500

    Add artist albums endpoint

--- src/routes/api/spotify.js
+++ src/routes/api/spotify.js
@@ -1,7 +1,8 @@
 const spotify = require('express').Router();
 const { spotifyApi } = require('../../utils/spotify');
-const { SPOTIFY_AUTH_URL, SCOPES } = require('../../constants/spotify');
 const { formatUrlParams } = require('../../utils/url');
+const { SPOTIFY_AUTH_URL, SCOPES } = require('../../constants/spotify');
+const status = require('../../constants/statusMessages');
 const log = require('../../utils/log');
 
 const clientId = process.env.SPOTIFY_CLIENT_ID;
@@ -30,9 +31,23 @@ spotify.get('/callback', async (req, res) => {
         refresh_token
       })
 
+      log.cool(`Redirecting to ${frontendUrl}/spotify/auth${params}`);
       res.redirect(`${frontendUrl}/spotify/auth${params}`);
     })
     .catch(err => log.error('Authorization Failed', err));
+});
+
+spotify.get('/artistAlbums', async (req, res) => {
+  const { query: { accessToken } } = req;
+  const artistId = '4xRYI6VqpkE3UwrDrAZL8L';
+
+  spotifyApi.setAccessToken(accessToken);
+
+  if (!accessToken) log.error('No access token');
+
+  spotifyApi.getArtistAlbums(artistId)
+    .then(data => status.success(res, { ...data.body }))
+    .catch(err => log.error('Failed to fetch user playlists', err));
 });
 
 module.exports = spotify;
