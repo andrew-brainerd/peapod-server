dfbf3cdb104b55566983985ab4a9e8a083aec4d6
Andrew Brainerd <drwb333@gmail.com>
2020-04-30 12:35:29 -0400

    Remove pod name

--- src/constants/pusher.js
+++ src/constants/pusher.js
@@ -1,5 +1,9 @@
 const NOW_PLAYING = 'nowPlaying';
+const MEMBER_ADDED = 'memberAdded';
+const LAUNCH_GAME = 'launchGame';
 
 module.exports = {
-  NOW_PLAYING
+  NOW_PLAYING,
+  MEMBER_ADDED,
+  LAUNCH_GAME
 };
--- src/data/pods.js
+++ src/data/pods.js
@@ -3,18 +3,18 @@ const log = require('../utils/log');
 const { messageTypes, sendSms } = require('../utils/messaging');
 const { PODS_COLLECTION } = require('../constants/collections');
 
-const createPod = async (name, createdBy) => {
-  const newPod = await data.insertOne(PODS_COLLECTION,
-    { name, createdBy, members: [createdBy] }
-  );
+const createPod = async (createdBy) => {
+  const newPod = await data.insertOne(PODS_COLLECTION, {
+    createdBy,
+    members: [createdBy]
+  });
 
-  log.success(`Created new pod ${newPod.name} (${newPod._id})`);
+  log.success(`Created new pod ${newPod._id}`);
 
   return newPod;
 };
 
 const sendInviteCode = async (podId, messageType, to) => {
-  const { name } = await getPod(podId);
   const frontendUrl = process.env.PEAPOD_UI_URL;
   const inviteLink = `${frontendUrl}/pods/${podId}`;
 
@@ -22,7 +22,7 @@ const sendInviteCode = async (podId, messageType, to) => {
     if (messageType === messageTypes.SMS) {
       try {
         sendSms(`You've been invited to a Peapod: ${inviteLink}`, to);
-        resolve({ message: `Sent invite link via ${messageType} for pod ${name} (${podId}) to ${to}` });
+        resolve({ message: `Sent invite link via ${messageType} for pod ${podId} to ${to}` });
       } catch (err) {
         const errorMessage = `Invalid messageType provided: [${messageType}]`;
         console.error(errorMessage);
@@ -38,58 +38,43 @@ const getPods = async (page, size, userId) =>
 const getPod = async podId => await data.getById(PODS_COLLECTION, podId);
 
 const deletePod = async podId => {
-  log.info(`Deleting pod (${podId})`);
+  log.cool(`Deleting pod ${podId}`);
   return await data.deleteOne(PODS_COLLECTION, podId);
 };
 
 const addMember = async (podId, user) => {
-  const { name } = await getPod(podId);
-  log.cool(`Adding member ${user.name} (${user.id}) to pod ${name} (${podId})`);
-  
+  log.cool(`Adding member ${user.id} to pod ${podId}`);
   return await data.addToSet(PODS_COLLECTION, podId, { members: user });
 };
 
 const removeMember = async (podId, user) => {
-  const { name } = await getPod(podId);
-  const { display_name: userName, id: userId } = user;
-  log.cool(`Removing member ${userName} (${userId}) from pod ${name} (${podId})`);
-  
+  log.cool(`Removing member ${user.id} from pod ${podId}`);
   return await data.pullFromSet(PODS_COLLECTION, podId, { members: user });
 };
 
 const addTrackToPlayQueue = async (podId, track) => {
-  const { name } = await getPod(podId);
-  log.cool(`Adding track ${track.name} to pod ${name} (${podId}) play queue`);
-
+  log.cool(`Adding track ${track.name} to pod ${podId} play queue`);
   return await data.addToSet(PODS_COLLECTION, podId, { queue: track });
 };
 
 const removeTrackFromPlayQueue = async (podId, track) => {
-  const { name } = await getPod(podId);
-  log.cool(`Removing track ${track.name} from pod ${name} (${podId}) play queue`);
-  
+  log.cool(`Removing track ${track.name} from pod ${podId} play queue`);
   return await data.pullFromSet(PODS_COLLECTION, podId, { queue: { id: track.id } });
 };
 
 const addTrackToPlayHistory = async (podId, track) => {
-  const { name } = await getPod(podId);
-  log.cool(`Adding track ${track.name} to pod ${name} (${podId}) play history`);
-  
+  log.cool(`Adding track ${track.name} to pod ${podId} play history`);
   return await data.addToSet(PODS_COLLECTION, podId, { history: track });
 };
 
 const addActiveMember = async (podId, user) => {
-  const { name } = await getPod(podId);
   const { display_name: userName, id: userId } = user;
-  log.cool(`Adding active member ${userName} (${userId}) to pod ${name} (${podId})`);
-
+  log.cool(`Adding active member ${userName} (${userId}) to pod ${podId}`);
   return await data.addToSet(PODS_COLLECTION, podId, { activeMembers: userId });
 };
 
 const removeActiveMember = async (podId, userId) => {
-  const { name } = await getPod(podId);
-  log.cool(`Removing active member ${userId} from pod ${name} (${podId})`);
-
+  log.cool(`Removing active member ${userId} from pod ${podId}`);
   return data.pullFromSet(PODS_COLLECTION, podId, { activeMembers: userId });
 };
 
